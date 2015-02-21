388e4e6ca2aa9812f9e00a3152109b32cfe80824
rosangelisgarcia <rosangelis.92@gmail.com>
2015-02-21 17:34:18 -0430

    Desastre de Jose

---
+++ src/Clases/Enlace.java
@@ -0,0 +1,19 @@
+package Clases;
+
+public class Enlace {
+
+	private String url;
+
+	public Enlace(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
