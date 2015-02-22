85cc27d950b9f6e53b84ef7159d08e80548d02c7
Jose Figueredo <josefigueredo@linuxmint.linuxmint-domain>
2015-02-22 12:50:57 -0430

    Enlace

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import Clases.Ciudad;
+import Clases.Enlace;
 import Clases.Promocion;
 
 
@@ -13,29 +14,36 @@ public class Main {
 
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
-	
+			
 		Ciudad c1 = new Ciudad();
-		c1.setNombre("Puerto Ordaz");
-		c1.setPais("Venezuela");
+		c1.setNombre("Toledo");
+		c1.setPais("España");
 				
 		Promocion p1 = new Promocion();
 		p1.setIdentificador(245638);
-		p1.setDescripcion("Excursion en Mochima");
+		p1.setDescripcion("Excursion en Merida");
 		p1.setPrecio_real(8700);
 		p1.setPrecio_ofertado(7600);
-//		p1.setFecha_ini_vig(new Date(5,3,2015));
-//		p1.setFecha_fin_vig(new Date(5,5,2015));
-//		p1.setCoordenadas(12345678901234567890);
+		p1.setFecha_ini_vig(new Date(115,2,3));
+		p1.setFecha_fin_vig(new Date(115,5,3));
+		p1.setCoordenadas(1234567890);
+		
+		Enlace e1 = new Enlace();
+		e1.setUrl("www.venezuelatuya.com");
+		
+		p1.getEnlaces().add(e1);
 
 		// Se agrega sobre la clase propietaria, check inverse en set de su xml
 		c1.getPromociones().add(p1);
+		p1.getCiudades().add(c1);
 		
-//        @SuppressWarnings("deprecation")
+        @SuppressWarnings("deprecation")
 	    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 	    Session session = sessionFactory.openSession();
 	    session.beginTransaction();
 
-	    // Primero se guarda la clase no propietaria, check inverse en set de su xml
+//	    // Primero se guarda la clase no propietaria, check inverse en set de su xml
+	    session.save(e1);
 	    session.save(p1);    
 	    session.save(c1);	
 	    
