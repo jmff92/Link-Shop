0c2822246217a8f4088b1323228e7d9401ca5670
rosangelisgarcia <rosangelis.92@gmail.com>
2015-02-22 13:01:23 -0430

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import Clases.Ciudad;
-import Clases.Promocion;
-
+import java.util.Set;
+import java.sql.Date;
+import java.util.HashSet;
+import Clases.*;
 
 public class Main {
 
@@ -30,15 +28,43 @@ public class Main {
 		// Se agrega sobre la clase propietaria, check inverse en set de su xml
 		c1.getPromociones().add(p1);
 		
+//Vale_Regalo vr1 = new Vale_Regalo();
+
+//vr1.setIdentificador(123456);
+//vr1.setFecha_compra(new Date(3,5,2015));
+//vr1.setForma_pago("TDC");
+//vr1.setDescrip_promo("Ful day en los Roques");
+//vr1.setCodigo_movil("QR123456");
+//vr1.setModo_envio("Correo Electronico");
+//vr1.setReceptor("Rosangelis");
+//vr1.setFecha_envio(new Date(5,5,2015));
+
+
+//Fecha_Realizacion fecha1 = new Fecha_Realizacion(new Date(115,5,2));
+//Fecha_Realizacion fecha2 = new Fecha_Realizacion(new Date(115,5,3));
+//Set<Fecha_Realizacion> fechas = new HashSet<Fecha_Realizacion>();
+//fechas.add(fecha1);
+//fechas.add(fecha2);
+
+//Evento evento = new Evento();
+//evento.setNombre("TOUR");
+//evento.setDescripcion("Rihanna concierto");
+//evento.setFechas(fechas);
+
+
 //        @SuppressWarnings("deprecation")
 	    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 	    Session session = sessionFactory.openSession();
 	    session.beginTransaction();
 
 	    // Primero se guarda la clase no propietaria, check inverse en set de su xml
+	   	//session.save(fecha1);
+	    //session.save(fecha2);
+		//session.save(evento);
+//	    session.save(vr1); 
 	    session.save(p1);    
 	    session.save(c1);	
-	    
+	    	    
         session.getTransaction().commit();
 	         
         session.close();
